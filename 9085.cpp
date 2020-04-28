6a57e0feedfe000cf71d117a9751e79a8d2b6cbf
parkseongsu <sspark516760@gmail.com>
2020-04-29 00:51:48 +0900

    9085

---
+++ 9085.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			int x; cin >> x;
+			sum += x;
+		}
+		cout << sum << "\n";
+	}
+}
