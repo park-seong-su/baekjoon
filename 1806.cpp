fda0b95e6aa8c1bdc2835adae27e40eee3b7f088
parkseongsu <sspark516760@gmail.com>
2020-07-01 22:28:47 +0900

    2003

--- 1806.cpp
+++ 1806.cpp
@@ -4,17 +4,21 @@
 using namespace std;
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
 	int n, s; cin >> n >> s;
 	vector<int> arr(n);
 	for (int i = 0; i < n; i++) cin >> arr[i];
 	int left = 0, right = 0, sum = arr[0], ans = 123456789;
 	while (left <= right && right < n) {
 		if (sum < s) {
-			right++;
-			if (right == n) break;
-			sum += arr[right];
+			sum += arr[++right];
 		}
-		else if (sum >= s) {
+		else if (sum == s) {
+			ans = min(ans, right - left + 1);
+			sum += arr[++right];
+		}
+		else if (sum > s) {
 			ans = min(ans, right - left + 1);
 			sum -= arr[left++];
 		}
